9a60d3fd0a5285e2d614c897d56919b83ade65ad
Jrace94 <jfra.ing28@mgmail.com>
2023-10-16 22:46:39 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints text
+ * Description: prints \"Programming is like building a multilingual puzzle to the stdout
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
