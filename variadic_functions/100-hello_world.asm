d5d31a4577b30600910cdbc607aa8b0f996f8cad
Jrace94 <jfra.ing28@mgmail.com>
2023-11-05 19:32:24 -0800

    Task 4

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,21 @@
-section .data
-	msg db "Hello, World",10
+; File: 100-hello_world.asm
+; Auth: Jose Rojas
+; Desc: 64-bit assembly program that prints
+;       Hello, World followed by a new line.
 
-	section .text
-	global main
+section .text
+   global main
 
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+   mov edx,len
+   mov ecx,msg
+   mov ebx,1
+   mov eax,4
+   int 0x80
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+   mov eax,0
+   int 0x80
+
+section .data
+   msg: db 'Hello, World', 0xa
+   len: equ $ - msg
